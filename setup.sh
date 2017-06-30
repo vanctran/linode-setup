2593ba9552f982019294a938d4d9ac266cc20451
Van Tran <vct@pdx.edu>
2017-06-30 08:56:49 +0000

    added -y for apt-get update

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 sudo bash -c 'echo "deb http://apt.linode.com/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/linode.list'
 wget -O- https://apt.linode.com/linode.gpg | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install linode-cli
+sudo apt-get install linode-cli -y
 linode configure
