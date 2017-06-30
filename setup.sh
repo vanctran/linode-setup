7145a08c3757c23751966684fbffdeffa6a9e7cd
Van Tran <vct@pdx.edu>
2017-06-30 08:49:35 +0000

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo bash -c 'echo "deb http://apt.linode.com/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/linode.list'
+wget -O- https://apt.linode.com/linode.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install linode-cli
+linode configure
